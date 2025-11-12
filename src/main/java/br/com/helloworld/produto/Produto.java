145c331e868e4d23cb356530755dbc531240fad2
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2025-11-11 21:05:06 -0300

    criação do projeto

---
+++ src/main/java/br/com/helloworld/produto/Produto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.helloworld.produto;
+
+/**
+ *
+ * @author humbe
+ */
+public class Produto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
